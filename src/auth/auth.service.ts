b733e9b68c8c7cf8b559f8e90876ece99cc9a34d
lidiaAsamnew <lidia.yirbe@gmail.com>
2026-04-20 12:40:48 +0300

    feat(auth): implement auth service with validate and login logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../user/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { UnauthorizedException } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  validateUser(username: string, password: string) {
+    const user = this.userService.findByUsername(username);
+
+    if (!user || user.password !== password) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+
+  login(user: any) {
+    const payload = { sub: user.id, username: user.username };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
