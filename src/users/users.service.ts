e91c45aa69ceaef043ecbe674b81f773fddcb7dc
lidiaAsamnew <lidia.yirbe@gmail.com>
2026-04-20 12:33:22 +0300

    feat(users): add user service with in-memory user lookup

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [
+        { id: 1, email: 'user1@gmail.com', password: 'user1' },
+        { id: 2, email: 'user2@gmail.com', password: 'user2' },
+    ];
+     findByUsername(username: string) {
+    return this.users.find(user => user.email === username);
+  }
+
+}
