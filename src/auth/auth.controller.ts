87fb9daea0e8c0c0d3a842e79bf1f06227116b29
lidiaAsamnew <lidia.yirbe@gmail.com>
2026-04-20 12:44:39 +0300

    feat(auth): add login endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+      
+    @Post('login')
+    login(@Body() dto: LoginDto) {
+    const user = this.authService.validateUser(dto.username, dto.password);
+    return this.authService.login(user);
+    }
+}
